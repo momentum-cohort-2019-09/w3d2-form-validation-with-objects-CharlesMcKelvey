d94e54f6caf01286a52cf454273999f4c9e5e9ca
Charles McKelvey <chazmckelvey@gmail.com>
2019-09-26 08:41:02 -0400

    Got things conceptually down and now it's the implementation

--- newMain.js
+++ newMain.js
@@ -8,11 +8,20 @@ function querys(selector) {
     return document.querySelectorAll(selector)
 }
 
+const validations = {
+    "name": [validatePresence], // 
+    "car-info": [validatePresence, validateCarInfoField], // 
+    "start-date": [validateDate], // 
+    "days": [validatePresence, validateDays], // 
+    "credit-card": [validatePresence, validateCredit], // 
+    "cvv": [validatePresence, validateCVV],
+    "expiration": [validateExpiration],
+}
+
 class Field {
-    construtor(input, validator) {
+    constructor(input, validator) {
         this.input = input
         this.validator = validator
-        this.value = input.value
     }
 
     markValid() {
@@ -34,13 +43,20 @@ class Field {
         parent.appendChild(childError)
     }
 
-    validate() {
+    getValue() {
+        // const input = this.input.value
+        return this.input.value
+    }
+
+    validate() { 
         const value = this.input.value
 
         if (this.validator(value)) {
             this.markValid()
+            return true
         } else {
             this.markInvalid()
+            return false
         }
     }
 
@@ -57,6 +73,41 @@ class Validate {
     }
 }
 
+class Form {
+    constructor() {
+        this.fields = []
+    }
+
+    addField(field) {
+        this.fields.push(field)
+    }
+
+    checkAll() {
+        let valid = true
+        // Loop through the fields in Form. If any are false, set valid to false
+        // 
+        for (let field of this.fields) {
+            let fieldIs = field.validate
+            if (!fieldIs) {
+                valid = false
+                break
+            }
+
+        }
+        return fieldIs // Could be true or false WHO KNOWS! 
+    }
+
+    createMyFields() {
+        let inputs = querys('input')
+        for(let input of inputs) {
+            let id = input.id
+            let newField = newField(input, validations[id])
+            this.fields.addField(newField)
+        }
+
+    }
+}
+
 const validatePresence = new Validate(function(value) {
     if (!value.trim()) {
         return false
@@ -65,13 +116,43 @@ const validatePresence = new Validate(function(value) {
     }
 }, "must be filled out")
 
+const validateCarInfoField = new Validate(function(value) {
+    if(value < 1900 || value > now.getFullYear()) {
+        return false
+    } else {
+        return true
+    }
+}, "must be a car year between 1900 and this year.")
 
+const validateDate = new Validate(function(value) {
+    let userDate = new Date(query('#start-date'))
+    let plusOne = userDate.getDate() + 1
+    userDate = userDate.setDate(plusOne)
+
+    if (userDate < now) {
+        return false
+    } else {
+        return true
+    }
+}, "must be in the future.")
+
+const validateDays = new Validate(function(value) {
+    if (value < 1 || value > 30) {
+        return false
+    } else {
+        return true
+    }
+}, "must be between 1 and 30.")
 
 const nameField = new Field(query('#name'), validatePresence)
+const carInfoField = new Field(query('#car-info'), validateCarInfoField)
 
-console.log(nameField)
-console.log(nameField.input, nameField.validator)
+
+
 document.querySelector('#parking-form').addEventListener('submit', (event) => {
     event.preventDefault()
-    
+
+    let form = new Form()
+    form.createMyFields()
+    form.checkAll()
 })
